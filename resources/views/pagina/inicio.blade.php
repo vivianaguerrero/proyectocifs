2bc0c9ea60599af8a3cdcfa086017b110dad9482
HernanAGarcia <hagarcial@uqvirtual.edu.co>
2018-06-05 20:38:08 -0500

    Configuración del FE

---
+++ resources/views/pagina/inicio.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+
+  @section('content')
+
+
+    <div class="container">
+
+    <h1>Control ingreso de personal a la finca la sierra</h1><br>
+
+  </div>
+    @endsection
